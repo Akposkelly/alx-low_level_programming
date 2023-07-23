82a1f48ad344f8d06677b9385463a9e82f6d865f
akposkelly <akposkelly@gmail.com>
2023-07-24 00:17:23 +0100

    8-print_base16.c

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ * Return: 0 Always (success)
+ */
+
+int main(void)
+{
+	int d;
+
+	for (d = '0'; d <= '9'; d++)
+	{
+		putchar(d);
+	}
+	putchar('\n');
+
+	return (0);
+}
