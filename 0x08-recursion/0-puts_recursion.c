3962892d87e6a93cb13e85b5931abd8d2984b8a8
akposkelly <akposkelly@gmail.com>
2023-08-02 13:13:04 +0100

    0-puts_recursion.c

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * _puts_recursion - prints a string, followed by a new line.
+ *
+ * @s: null terminated string to print
+ */
+
+void _puts_recursion(char *s)
+{
+	puts(s);
+}
---
+++ 0x08-recursion/main.h
@@ -0,0 +1,20 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <unistd.h>
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+
+int wildcmp(char *s1, char *s2);
+int is_palindrome(char *s);
+int is_prime_number(int n);
+int _sqrt_recursion(int n);
+int _pow_recursion(int x, int y);
+int factorial(int n);
+int _strlen_recursion(char *s);
+void _print_rev_recursion(char *s);
+int _putchar(char c);
+void _puts_recursion(char *s);
+
+#endif
