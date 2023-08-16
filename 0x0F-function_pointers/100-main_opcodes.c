4cef66f9c971dfb7e529ed82fb09d009423d8098
akposkelly <akposkelly@gmail.com>
2023-08-16 22:59:25 +0100

    all files

---
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -0,0 +1,38 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * main - Entry point for the program.
+ * @argc: Number of arguments.
+ * @argv: Argument vector.
+ *
+ * Return: 0 for success.
+ */
+int main(int argc, char *argv[])
+{
+	int i, inputSize;
+
+	if (argc != 2)
+	{
+		printf("Error\n");
+		exit(1);
+	}
+
+	inputSize = atoi(argv[1]);
+	if (inputSize < 0)
+	{
+		printf("Error\n");
+		exit(2);
+	}
+
+	for (i = 0; i < inputSize; i++)
+	{
+		printf("%02hhx", *((char *)main + i));
+		if (i < inputSize - 1)
+			printf(" ");
+		else
+			printf("\n");
+	}
+
+	return (0);
+}
---
+++ 0x0F-function_pointers/2-int_index.c
@@ -0,0 +1,30 @@
+#include "function_pointers.h"
+
+/**
+ * int_index - searches for an integer
+ * @array: array to search
+ * @size: number of elements in array
+ * @cmp: function to compare values
+ * Return: returns index if found or not
+ */
+
+int int_index(int *array, int size, int (*cmp)(int))
+{
+	int i, index = -1; /* Initialize index to -1 */
+
+	if (size > 0 && cmp && array)
+	{
+		for (i = 0; i < size; i++)
+		{
+			if (cmp(array[i]) != 0)
+			{
+				/* Update index only when a match is found */
+				index = i;
+				break;
+			}
+		}
+	}
+
+	/* Return the correct index or -1 if no match found */
+	return (index);
+}
